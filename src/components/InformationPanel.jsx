507a805b14ae6f9262373060420b10edc5640d14
ZTW21 <ztwilson2002@gmail.com>
2024-02-16 10:32:51 -0500

    final design changes

--- src/components/InformationPanel.jsx
+++ src/components/InformationPanel.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getPokemonHabitat, getPokemonInfoByUrl, getPokemonLocation } from "../server/api";
 import { getImgUrl } from "../until";
-const listClass = 'ml-10 pl-5 border-2 border-slate-200 w-1/3'
+const listClass = 'ml-10 pl-5 border-2 border-slate-200 w-1/3 border-border-color text-2xl'
 /**@typedef {{name: string, url: string}} InfoType*/
 /**@typedef {{is_hidden: boolean, ability: InfoType}} AbilityType*/
 /**@typedef {{front_default: string,front_shiny: string }} SpritesType*/
@@ -63,30 +63,42 @@ function InformationPanel({ selectedPokemon }) {
 	return (
 		<div style={gradientStyle} className='p-4 min-h-screen'>
 			<img src={getImgUrl(selectedPokemon.url)} alt={selectedPokemon.name} className='w-40 h-40 mx-auto' />
-			<h2 className='text-2xl text-center mt-2 uppercase'>{selectedPokemon.name}</h2>
+			<h2 className='font-bold text-3xl text-center mt-2 uppercase pb-4'>{selectedPokemon.name}</h2>
 			{isLoading || Object.keys(pokemonInfo).length === 0 ? (
 				<h3>Loading....</h3>
 			) : (
 				<>
-					<div>Weight: {pokemonInfo.weight}</div>
-					<div>Height: {pokemonInfo.height}</div>
-					<div>Experience: {pokemonInfo.base_experience}</div>
-					<div>Location: {pokemonInfo.habitat || '...'}</div>
-					<div>Forms: {pokemonInfo.forms.map((item, index) => {
+					<div className='flex flex-auto'>
+						<div className='font-bold text-2xl'>Weight:</div>
+						<div className="text-2xl">{pokemonInfo.weight}</div>
+					</div>
+					<div className='flex flex-auto'>
+						<div className='font-bold text-2xl'>Height:</div>
+						<div className="text-2xl">{pokemonInfo.height}</div>
+					</div>
+					<div className='flex flex-auto'>
+						<div className='font-bold text-2xl'>Experience:</div>
+						<div className="text-2xl">{pokemonInfo.base_experience}</div>
+					</div>
+					<div className='flex flex-auto'>
+						<div className='font-bold text-2xl'>Location:</div>
+						<div className="text-2xl">{pokemonInfo.habitat || '...'}</div>
+					</div>
+					
+					<div className='font-bold text-2xl'>Forms:</div> <div>{pokemonInfo.forms.map((item, index) => {
 						return <div key={index} className={listClass}>{item.name}</div>
 					})}</div>
 					<div>
-						Abilities:{" "}
+					<div className='font-bold text-2xl'>Abilities:{" "}</div>
 						{pokemonInfo.abilities.map((item, index) => {
 							return (
 								<div key={index} className={listClass}>{item.ability.name}</div>
 							);
 						})}
 					</div>
-					<div>Moves: {pokemonInfo.moves.slice(0, 5).map((item, index) => {
+					<div className='font-bold text-2xl'>Moves:</div> <div className="">{pokemonInfo.moves.slice(0, 5).map((item, index) => {
 						return <div key={index} className={listClass}>{item.move.name}</div>
 					})}</div>
-					<div></div>
 				</>
 			)}
 		</div>
